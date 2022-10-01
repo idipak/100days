4b9bd3adbfd4471a7266706e78efe35b9f7a29d4
Dipak <dipak_pd@outlook.com>
2022-10-01 11:39:03 +0530

    Initial Commit

---
+++ HundredDays/HundredDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  HundredDaysApp.swift
+//  HundredDays
+//
+//  Created by Codebucket on 01/10/22.
+//
+
+import SwiftUI
+
+@main
+struct HundredDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
